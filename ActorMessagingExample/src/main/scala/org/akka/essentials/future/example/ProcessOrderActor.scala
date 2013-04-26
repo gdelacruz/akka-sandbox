98bcaa4fc2570003264d995256f206e41b3bc81f
German de la Cruz <german.delacruz@gmail.com>
2013-04-26 12:53:03 -0300

    actualizacion a akka 2.1.2 con scala 2.10.1

--- ActorMessagingExample/src/main/scala/org/akka/essentials/future/example/ProcessOrderActor.scala
+++ ActorMessagingExample/src/main/scala/org/akka/essentials/future/example/ProcessOrderActor.scala
@@ -4,22 +4,30 @@ import akka.actor.Props
 import akka.pattern.ask
 import akka.pattern.pipe
 import akka.util.Timeout
-import akka.util.duration._
-import akka.dispatch.Future
+import akka.pattern.ask
+import akka.actor.actorRef2Scala
+import scala.concurrent.duration._
+import scala.concurrent.Future
+
+
 
 class ProcessOrderActor extends Actor {
 
-  implicit val timeout = Timeout(5 seconds)
+  implicit val timeout = Timeout(5.seconds)
   val orderActor = context.actorOf(Props[OrderActor])
   val addressActor = context.actorOf(Props[AddressActor])
   val orderAggregateActor = context.actorOf(Props[OrderAggregateActor])
 
   def receive = {
     case userId: Integer =>
+      //Tuve que poner este import. Lo saque de la doc de scala 2.1. No entiendo muy bien porque
+      //http://doc.akka.io/docs/akka/snapshot/scala/futures.html
+      import context.dispatcher
+      
       val aggResult: Future[OrderHistory] =
         for {
-          order <- ask(orderActor, userId).mapTo[Order] // call pattern directly
-          address <- addressActor ask userId mapTo manifest[Address] // call by implicit conversion
+          order <- (orderActor ? userId).mapTo[Order] // call pattern directly
+          address <- (addressActor ? userId) mapTo manifest[Address] // call by implicit conversion
         } yield OrderHistory(order, address)
       aggResult pipeTo orderAggregateActor
   }
--- ActorMessagingExample/src/main/scala/org/akka/essentials/hotswap/example/PingPongActor.scala
+++ ActorMessagingExample/src/main/scala/org/akka/essentials/hotswap/example/PingPongActor.scala
@@ -2,8 +2,8 @@ package org.akka.essentials.hotswap.example
 
 import akka.actor.Actor
 
-case class PING
-case class PONG
+case class PING()
+case class PONG()
 
 class PingPongActor extends Actor {
   import context._
@@ -13,13 +13,13 @@ class PingPongActor extends Actor {
       println("PING")
       count = count + 1
       Thread.sleep(100)
-      self ! PONG
+      self ! PONG()
       become {
         case PONG =>
           println("PONG")
           count = count + 1
           Thread.sleep(100)
-          self ! PING
+          self ! PING()
           unbecome()
       }
       if(count > 10) context.stop(self)
